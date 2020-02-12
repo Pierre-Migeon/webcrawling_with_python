1ca5c061c50be02d52eb60e2fa4bdc398c3d20b5
Pierre-Migeon <p.migeon@gmail.com>
2020-02-12 14:13:10 -0800

    bash scripting a bit

--- find_urls.py
+++ find_urls.py
@@ -1,22 +1,21 @@
-
+import os
 
 '''
 This portion finds URLs that may contain information about housing prices in the San Francisco Bay area and stores them in a textfile.
 '''
 
 try: 
-    from googlesearch import search
+	from googlesearch import search
 except ImportError:
-    print("No module named 'google' found, trying to download now")
-    system("pip install beautifulsoup4")
-    system("pip install google")
+	print("No module named 'google' found, trying to download now")
+	os.system("pip3 install beautifulsoup4")
+	os.system("pip3 install google")
 
-# to search 
+# to search
 query = "Bay Area Housing Prices"
-file = open("urls.txt", "w")
+file = open("_urls.txt", "w")
 
-for j in search(query, tld="com", lang='en', num=10): 
-    file.write(j + "\n")
+for j in search(query, tld="com", lang='en', num=10, pause = 2.0):
+	file.write(j + "\n")
 
 file.close()
-
--- run.sh
+++ run.sh
@@ -1,2 +1,30 @@
-python3 find_urls.py 
+#########################
+# Playing with data...	#
+#	Pierre Migeon	#
+#########################
+URL_FILE="./urls.txt"
+
+#If there are no urls...
+if [ ! -f $URL_FILE ] || [ ! -s $URL_FILE ]; then
+	python3 find_urls.py
+fi;
+
+#if there still aren't URLs, then exit... 
+if [ ! -f $URL_FILE ] ||  [ ! -s $URL_FILE ]; then
+	echo "There was a problem creating the list of URLs. Something went wrong, exiting..." && exit 1
+fi;
+
+#If there's nothing in the database, then make the database
+TEST=`python3 testdb.py | wc -l`
+if [ $TEST == "0" ]; then
+	python3 makedb.py
+fi;
+
+#If there's still nothing in the database, something went wrong and we'll exit.
+TEST=`python3 testdb.py | wc -l`
+if [ $TEST == "0" ]; then 
+	echo "There were no entries in the url database. Something went wrong... exiting now..." && exit 1;
+fi;
+
 scrapy runspider scraper.py
+
